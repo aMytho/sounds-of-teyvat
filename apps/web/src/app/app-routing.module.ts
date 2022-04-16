ac764860fc6c703f86073b78a5bed11819c71e78
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2022-04-15 22:38:22 -0500

    Add routes

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 const routes: Routes = [
     { path: 'covers', loadChildren: () => import('./covers/covers.module').then(m => m.CoversModule) },
+    { path: 'signin', loadChildren: () => import('./signin/signin.module').then(m => m.SigninModule) },
+    { path: 'login', loadChildren: () => import('./signin/signin.module').then(m => m.SigninModule) },
+    { path: "info", loadChildren: () => import('./site-info/site-info.module').then(m => m.SiteInfoModule) },
     { path: '', component: HomeComponent },
     { path: '**', component: PageNotFoundComponent }
 ]
