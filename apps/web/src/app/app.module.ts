2755ef50537b149bb68d877a81c67bdca0de0bda
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2022-04-16 23:12:26 -0500

    404 page

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { NxWelcomeComponent } from "./nx-welcome.component";
 import { HttpClientModule } from "@angular/common/http";
 import { NavComponent } from "./nav/nav.component";
 import { RoutingModule } from "./app-routing.module";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, NavComponent],
+    declarations: [AppComponent, NxWelcomeComponent, NavComponent, PageNotFoundComponent],
     imports: [BrowserModule, HttpClientModule, RoutingModule],
     providers: [],
     bootstrap: [AppComponent],
